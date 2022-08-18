72a420cddcee435a0ff86b9c63639ff2cd4229e0
oceanzhangyang <zhang51848254@163.com>
2022-08-18 13:01:45 +0800

    init app

---
+++ app/views.py
@@ -0,0 +1,81 @@
+from flask import render_template
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder import ModelView, ModelRestApi,expose,BaseView,has_access
+
+from . import appbuilder, db
+
+class MyView(BaseView):
+    route_base = "/myview"
+
+    @expose('/method1/')
+    @has_access
+    def method1(self):
+        return 'Hello'
+
+    @expose('/method2/<string:param1>')
+    @has_access
+    def method2(self, param1):
+        # do something with param1
+        # and render it
+        param1 = 'Hello %s' % (param1)
+        return param1
+
+    @expose('/method3/<string:param1>')
+    @has_access
+    def method3(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        self.update_redirect()
+        return self.render_template('method3.html', param1 = param1)
+
+
+"""
+    Create your Model based REST API::
+
+    class MyModelApi(ModelRestApi):
+        datamodel = SQLAInterface(MyModel)
+
+    appbuilder.add_api(MyModelApi)
+
+
+    Create your Views::
+
+
+    class MyModelView(ModelView):
+        datamodel = SQLAInterface(MyModel)
+
+
+    Next, register your Views::
+
+
+    appbuilder.add_view(
+        MyModelView,
+        "My View",
+        icon="fa-folder-open-o",
+        category="My Category",
+        category_icon='fa-envelope'
+    )
+"""
+
+"""
+    Application wide 404 error handler
+"""
+
+
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return (
+        render_template(
+            "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
+        ),
+        404,
+    )
+
+
+db.create_all()
+
+appbuilder.add_view(MyView, "My View")
+appbuilder.add_link("Method1", href='/myview/method1', category='My View')
+appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
+appbuilder.add_link("Method3", href='/myview/method3/john', category='My View')
